9f2404ca97f131f8984539ccc5f5c7f4f2f7519b
Salaheldin <salaheldin@gmail.com>
2018-03-27 16:12:55 +0200

    javascript added search function without category

--- js/code.js
+++ js/code.js
@@ -1,21 +1,21 @@
 var books = [
 	{
 	"id": "book_1",
-	"name": "",
+	"name": "python",
 	"category":"",
 	"picture_url":"",
 	"price": 0
 	},
 						{
 						"id": "book_2",
-						"name": "",
+						"name": "php",
 						"category":"",
 						"picture_url":"",
 						"price": 0
 						},
 											{
 											"id": "book_1",
-											"name": "",
+											"name": "javascript",
 											"category":"",
 											"picture_url":"",
 											"price": 0
@@ -26,21 +26,21 @@ var books = [
 var albums = [
 	{
 	"id": "album_1",
-	"name": "",
+	"name": "metallica",
 	"category":"",
 	"picture_url":"",
 	"price": 0
 	},
 						{
 						"id": "album_2",
-						"name": "",
+						"name": "garage inc",
 						"category":"",
 						"picture_url":"",
 						"price": 0
 						},
 											{
 											"id": "album_1",
-											"name": "",
+											"name": "hardwired",
 											"category":"",
 											"picture_url":"",
 											"price": 0
@@ -50,21 +50,21 @@ var albums = [
 var movies = [
 	{
 	"id": "movie_1",
-	"name": "",
+	"name": "casper",
 	"category":"",
 	"picture_url":"",
 	"price": 0
 	},
 						{
 						"id": "movie_2",
-						"name": "",
+						"name": "the mask",
 						"category":"",
 						"picture_url":"",
 						"price": 0
 						},
 											{
 											"id": "movie_1",
-											"name": "",
+											"name": "home alone",
 											"category":"",
 											"picture_url":"",
 											"price": 0
@@ -72,5 +72,28 @@ var movies = [
 ];
 
 
-
-
+$(function(){
+	$("#search").submit(function(e) { 
+        var value = $("#search-input").val();
+        e.preventDefault();
+        for (var i = 0; i < books.length; i++) {
+        	if (value === books[i].name){
+        		alert("Search found in books");
+        		return;
+        	}
+        }
+        for (var i = 0; i < movies.length; i++) {
+        	if (value === movies[i].name){
+        		alert("Search found in movies");
+        		return;
+        	}
+        }
+        for (var i = 0; i < albums.length; i++) {
+        	if (value === albums[i].name){
+        		alert("Search found in albums");
+        		return;
+        	}
+        }
+        alert("search not found");		
+    });
+});
