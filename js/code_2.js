39f92cf28426778586040f89fa3f133e54558c41
Salaheldin <salaheldin@gmail.com>
2018-03-28 22:03:59 +0200

    created addToPage function

--- js/code_2.js
+++ js/code_2.js
@@ -1,7 +1,7 @@
 var books = [
 	{
 	"id": "book_1",
-	"name": "python",
+	"name": "Python",
 	"category":"",
 	"picture_url":"images/python.jpg",
 	"price": 0
@@ -14,8 +14,8 @@ var books = [
 						"price": 0
 						},
 											{
-											"id": "book_1",
-											"name": "javascript",
+											"id": "book_3",
+											"name": "JavaScript",
 											"category":"",
 											"picture_url":"images/javascript.jpg",
 											"price": 0
@@ -26,21 +26,21 @@ var books = [
 var albums = [
 	{
 	"id": "album_1",
-	"name": "metallica",
+	"name": "Metallica",
 	"category":"",
 	"picture_url":"images/metallica.jpg",
 	"price": 0
 	},
 						{
 						"id": "album_2",
-						"name": "garage inc",
+						"name": "Garage Inc",
 						"category":"",
 						"picture_url":"images/garage_inc.jpg",
 						"price": 0
 						},
 											{
-											"id": "album_1",
-											"name": "hardwired",
+											"id": "album_3",
+											"name": "Hardwired",
 											"category":"",
 											"picture_url":"images/hardwired.jpg",
 											"price": 0
@@ -50,42 +50,73 @@ var albums = [
 var movies = [
 	{
 	"id": "movie_1",
-	"name": "casper",
+	"name": "Casper",
 	"category":"",
 	"picture_url":"images/casper.jpg",
 	"price": 0
 	},
 						{
 						"id": "movie_2",
-						"name": "the mask",
+						"name": "The Mask",
 						"category":"",
 						"picture_url":"images/the_mask.jpg",
 						"price": 0
 						},
 											{
-											"id": "movie_1",
-											"name": "home alone",
+											"id": "movie_3",
+											"name": "Home Alone",
 											"category":"",
 											"picture_url":"images/home_alone.jpg",
 											"price": 0
 											}
 ];
 
-$(window).on('load', function() {
+
+$(window).on('load', function(){
+	addToPage();
+});
+
+function addToPage(){
 	for (var i = 0; i < books.length; i++) {
 		$('#book-'+[i+1]+' .name').text(books[i].name);
 		$('#book-'+[i+1]+' .img').attr("src",books[i].picture_url);
 	}
-	for (var j = 0; j < books.length; j++) {
+	for (var j = 0; j < albums.length; j++) {
 		$('#album-'+[j+1]+' .name').text(albums[j].name);
 		$('#album-'+[j+1]+' .img').attr("src",albums[j].picture_url);
 	}
-	for (var k = 0; k < books.length; k++) {
+	for (var k = 0; k < movies.length; k++) {
 		$('#movie-'+[k+1]+' .name').text(movies[k].name);
 		$('#movie-'+[k+1]+' .img').attr("src",movies[k].picture_url);
 	}
 
-});
+};
+
+
+
+
+
+
+
+
+
+
+
+// $(window).on('load', function() {
+// 	for (var i = 0; i < books.length; i++) {
+// 		$('#book-'+[i+1]+' .name').text(books[i].name);
+// 		$('#book-'+[i+1]+' .img').attr("src",books[i].picture_url);
+// 	}
+// 	for (var j = 0; j < books.length; j++) {
+// 		$('#album-'+[j+1]+' .name').text(albums[j].name);
+// 		$('#album-'+[j+1]+' .img').attr("src",albums[j].picture_url);
+// 	}
+// 	for (var k = 0; k < books.length; k++) {
+// 		$('#movie-'+[k+1]+' .name').text(movies[k].name);
+// 		$('#movie-'+[k+1]+' .img').attr("src",movies[k].picture_url);
+// 	}
+
+// });
 
 
 
